7436ae853660053387119a956f73cab4c596d202
kabaya2 <kabayawright@gmail.com>
2026-07-09 21:23:31 +0300

    Update profileContext.jsx

--- src/context/profileContext.jsx
+++ src/context/profileContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useRef, useState } from "react";
 
-const API_URL = process.env.REACT_APP_API_URL || "https://digitalblitz-backend.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL || "https://lumostra-admin.onrender.com";
 
 const ProfileContext = createContext({
   profile: null,
