3470d41f45415f42bab87b7c8b871caa87ba6385
kabaya2 <kabayawright@gmail.com>
2026-07-09 21:24:20 +0300

    Update BindWallet.jsx

--- src/pages/BindWallet.jsx
+++ src/pages/BindWallet.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const BACKEND_API = "https://digitalblitz-backend.onrender.com/api";
+const BACKEND_API = "https://lumostra-admin.onrender.com/api";
 
 export default function BindWallet() {
   const [fullName, setFullName] = useState("");
