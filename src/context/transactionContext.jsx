1772d07219b93252c6ebd74c9ab544e9b71156f1
kabaya2 <kabayawright@gmail.com>
2026-07-09 21:23:49 +0300

    Update transactionContext.jsx

--- src/context/transactionContext.jsx
+++ src/context/transactionContext.jsx
@@ -6,7 +6,7 @@ export const TransactionProvider = ({ children }) => {
   const [deposits, setDeposits] = useState([]);
   const [withdrawals, setWithdrawals] = useState([]);
   const [loading, setLoading] = useState(false);
-  const BASE_URL = "https://digitalblitz-backend.onrender.com";
+  const BASE_URL = "https://lumostra-admin.onrender.com";
 
   const fetchTransactions = async () => {
     const token = localStorage.getItem("authToken");
