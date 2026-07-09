106e9626170a88d30676a5b4c146c937a4b0c372
kabaya2 <kabayawright@gmail.com>
2026-07-09 21:26:16 +0300

    Update Notifications.jsx

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const BACKEND_URL = "https://digitalblitz-backend.onrender.com";
+const BACKEND_URL = "https://lumostra-admin.onrender.com";
 const START_BLUE = "#1fb6fc";
 const HEADER_HEIGHT = 64;
 
