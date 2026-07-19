790bd19b63b15a9ee17c766e3f4d044f7da8f504
kabaya2 <kabayawright@gmail.com>
2026-07-19 21:41:52 +0300

    Update balanceContext.jsx

--- src/context/balanceContext.jsx
+++ src/context/balanceContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 
-const BASE_URL = "https://lumostra-admin.onrender.com";
+const BASE_URL = "https://lumostra-admins.onrender.com";
 
 export const BalanceContext = createContext();
 
