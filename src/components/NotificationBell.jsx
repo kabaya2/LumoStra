b702e444ade0a63618c08a6ac98683592728b69d
kabaya2 <kabayawright@gmail.com>
2026-07-19 21:39:52 +0300

    Update NotificationBell.jsx

--- src/components/NotificationBell.jsx
+++ src/components/NotificationBell.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const RENDER_BACKEND_URL = "https://lumostra-admin.onrender.com";
+const RENDER_BACKEND_URL = "https://lumostra-admins.onrender.com";
 
 export default function NotificationBell() {
   const [unread, setUnread] = useState(false);
